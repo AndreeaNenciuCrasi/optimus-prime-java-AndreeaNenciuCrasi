bc87f5284fc8127d2a21c42fe448cb4fc236649e
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-06-04 12:33:19 +0300

    Adds simple find prime numbers algorithm

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,8 +1,31 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Integer> getPrimes = getPrimes(200);
+        for (int i = 0; i < getPrimes.size(); i++) {
+            System.out.print(getPrimes.get(i) + " ");
+        }
+    }
+
+    static List<Integer> getPrimes(int n) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        for (int k = 2; k <= n; k++) {
+            int counter = 0;
+            for (int i = 1; i <= k; i++) {
+                if ((k % i) == 0) {
+                    counter++;
+                }
+            }
+            if (counter == 2) {
+                primeNumbers.add(k);
+            }
+        }
+        return primeNumbers;
     }
 }
